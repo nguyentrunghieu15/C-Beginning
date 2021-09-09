0512d900789e90ea99f81e6a9de3e0b46d532023
nguyentrunghieu15 <78260236+nguyentrunghieu15@users.noreply.github.com>
2021-09-09 10:44:28 -0400

    sua

--- luyenfile.cpp
+++ luyenfile.cpp
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+
 
 typedef struct sv
 {
-	char hoten[20];
+	char hoten[50];
 	char birth[20];
 	char que[20];
 }sv;
-void nhapthongtin(sv bk[],int n)
-{
-	int i;
-	for(i=0;i<=n;i++);
-	{
-		printf("Nhap ten sinh vien thu %d :",i+1);
-		fflush(stdin);
-		gets(bk[i].hoten);
-		printf("Nhap ngay sinh(dd/mm/yyyy) :");
-		fflush(stdin);
-		gets(bk[i].birth);
-		printf("Nhap que quan :");
-		fflush(stdin);
-		gets(bk[i].que);
-	}
-}
 
+void nhapthongtin(sv bk[],int n);
 int main()
 {
-	int n,i;
+	int n,a;
 	printf("Nhap so luong sinh vien:");
-	scanf("%d", &n);
-	sv bk[n];
+	scanf("%d%*c", &n);
+	sv bk[100];
 	nhapthongtin(bk,n);
-	for(i=0;i<n;i++)
-	    printf("%20s %20s %20s\n",bk[i].hoten, bk[i].birth, bk[i].que);
+	for(a=0;a<n;a++)
+	    {printf("%20s",bk[a].hoten);
+        printf("%20s",bk[a].birth);
+        printf("%20s\n",bk[a].que);
+        }
 	return 0;
+}
+void nhapthongtin(sv bk[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("Nhap ten sinh vien thu %d :",i+1);
+		fgets(bk[i].hoten,50,stdin);
+		printf("Nhap ngay sinh(dd/mm/yyyy) :");
+		fgets(bk[i].birth,20,stdin);
+		printf("Nhap que quan :");
+		fgets(bk[i].que,20,stdin);
+	}
 }
